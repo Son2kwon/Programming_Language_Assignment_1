9700db8fc16a01094366803933f274dd99e7fa60
Son2kwon <paulson01@naver.com>
2023-11-05 00:23:04 +0900

    임시

--- 20223515/main.cpp
+++ 20223515/main.cpp
@@ -1,15 +1,24 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<sstream>
 #include"Parser.h"
 
 int main(int args, char* argv[]) {
 	ifstream f(argv[1]);
 	string line;
+	string* token;
 
 	if (f.is_open()) {
 		while (getline(f, line)) {
+			stringstream ss(line);
+			int index = 0;
 
+			cout << line << endl;
+			
+			while (ss >> token[index]) {
+				
+			}
 		}
 	}
 	else cout << "Fail to open file" << endl;
